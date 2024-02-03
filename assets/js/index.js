05eb5c56256e93a17bc1776e78c01aa4b1116edd
filyar0306 <sd81n8nsk@code.edu.az>
2024-02-04 01:28:02 +0400

    second commit

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,23 +1,45 @@
-$(".s-carousel").owlCarousel({
-    margin: 0,
-    loop: true,
-    autoplay: true,
-    autoplaySpeed: 2000,
-    autoplayTimeout: 2000,
-    autoplayHoverPause: true,
-    slideTransition: 'linear',
-    responsive: {
-        0:{
-        items:2,
-        nav: false
-        },
-        800:{
-        items:4,
-        nav: false
-        },
-        1000:{
-        items:6,
-        nav: false
+// const theme__btn = document.getElementById("theme__btn")
+// theme__btn.addEventListener("click", function(){
+//     document.body.classList.toggle("light-theme");
+//     localStorage.setItem("mode", document.body.classList)
+// })
+// if (localStorage.getItem("mode") != "") {
+
+//      document.body.classList.add(localStorage.getItem("mode"));
+// }
+
+document.addEventListener('DOMContentLoaded', function () {
+    const carouselItems = document.querySelectorAll('.carousel-item');
+    let currentIndex = 0;
+  
+    function showItem(index) {
+      carouselItems.forEach((item, i) => {
+        if (i === index) {
+          item.classList.add('active');
+        } else {
+          item.classList.remove('active');
         }
+      });
     }
-});
+  
+    function nextItem() {
+      currentIndex = (currentIndex + 1) % carouselItems.length;
+      showItem(currentIndex);
+    }
+  
+    function prevItem() {
+      currentIndex = (currentIndex - 1 + carouselItems.length) % carouselItems.length;
+      showItem(currentIndex);
+    }
+  
+    setInterval(nextItem, 5000); // Auto slide every 5 seconds
+  
+    // Optionally, you can add event listeners for left and right arrow keys
+    document.addEventListener('keydown', function (event) {
+      if (event.key === 'ArrowRight') {
+        nextItem();
+      } else if (event.key === 'ArrowLeft') {
+        prevItem();
+      }
+    });
+  });
